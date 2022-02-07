72a57ad0e2d72a43d2ae495749337862f42e08a0
Aleksey <burovalexprogrammer@gmail.com>
2022-02-07 08:50:30 +0200

    Implemented platform's control

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    //public SimpleAudioEvent simpleAudioEvent;
+    public float power = 300f;
+    public Transform forcePoint;
+    public float forceRadius;
+
+    void OnUse() {
+        Debug.Log("Use");
+        var audioSource = GetComponent<AudioSource>();
+        //simpleAudioEvent.Play(audioSource);
+
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.transform.CompareTag("Ball")) {
+            collision.rigidbody.AddExplosionForce(power, forcePoint.position, forceRadius);
+        }
+    }
+
+
+}
